45c23f5317d9f47178c85dcf1eb403a79eb6a86c
miyakogi <miyako.dev@gmail.com>
2018-03-15 09:57:00 +0900

    Smaller dodo file

--- dodo.py
+++ dodo.py
@@ -1,26 +1,10 @@
-
-DOIT_CONFIG = {
-    'default_tasks': [
-        'flake8', 'unittest',
-    ],
-}
-
-
 def task_flake8():
-    """Run flake8 check."""
-    return {
-        'actions': ['flake8 a.py'],
-    }
+    return {'actions': ['flake8 a.py']}
 
 
 def task_unittest():
-    return {
-        'actions': ['python -m unittest']
-    }
+    return {'actions': ['python -m unittest']}
 
 
 def task_all():
-    return {
-        'actions': None,
-        'task_dep': ['flake8', 'unittest']
-    }
+    return {'actions': None, 'task_dep': ['flake8', 'unittest']}
